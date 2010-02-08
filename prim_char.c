84c401ce436e2301fff2c180df05f1e0a2899902
Bob Miller <kbob@jogger-egg.com>
2010-02-07 23:26:34 -0800

    Implemented char?.

--- prim_char.c
+++ prim_char.c
@@ -6,6 +6,11 @@ static inline bool is_Unicode_scalar_value(int n)
     return (0 <= n && n <= 0xd7ff) || (0xe000 <= n && n <= 0x10ffff);
 }
 
+DEFINE_PROC(L"char?", 1)(obj_t obj)
+{
+    return make_boolean(is_character(obj));
+}
+
 DEFINE_PROC(L"char->integer", 1)(obj_t chr)
 {
     return make_fixnum(character_value(chr));
