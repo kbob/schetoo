05202f1075becaa5e9f8d0a9e237738714aa2eba
Bob Miller <kbob@jogger-egg.com>
2010-02-07 23:22:23 -0800

    Implemented symbol?.

--- prim_sym.c
+++ prim_sym.c
@@ -1,6 +1,11 @@
 #include "prim.h"
 #include "types.h"
 
+DEFINE_PROC(L"symbol?", 1)(obj_t obj)
+{
+    return make_boolean(is_symbol(obj));
+}
+
 DEFINE_PROC(L"symbol->string", 1)(obj_t symbol)
 {
     return symbol_name(symbol);
